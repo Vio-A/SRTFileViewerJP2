ab3f4c5d2695b7cfaaf38125775a962dacfa1c29
Vio-A <sebastianrumple@gmail.com>
2025-04-07 18:48:35 +0930

    a

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from theme import get_black_white_theme
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    app.setStyleSheet(get_black_white_theme()
+    app.setStyleSheet(get_black_white_theme())
     viewer = SubtitleViewer()
     viewer.show()
     sys.exit(app.exec_())
